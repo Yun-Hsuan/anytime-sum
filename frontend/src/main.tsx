035b07a3e919a3afc54b08694dfa382e16cc268f
TsangLin Chou <tsanglin.chou@4saitech.com>
2025-02-18 05:59:30 +0800

    Initial commit

---
+++ frontend/src/main.tsx
@@ -0,0 +1,33 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { RouterProvider, createRouter } from "@tanstack/react-router"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { routeTree } from "./routeTree.gen"
+
+import { StrictMode } from "react"
+import { OpenAPI } from "./client"
+import { CustomProvider } from "./components/ui/provider"
+
+OpenAPI.BASE = import.meta.env.VITE_API_URL
+OpenAPI.TOKEN = async () => {
+  return localStorage.getItem("access_token") || ""
+}
+
+const queryClient = new QueryClient()
+
+const router = createRouter({ routeTree, context: { queryClient } })
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router
+  }
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <CustomProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </CustomProvider>
+  </StrictMode>,
+)
